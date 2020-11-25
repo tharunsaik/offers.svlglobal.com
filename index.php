3866cdc41a9251b76fb3662fbe351caa7a7d02a3
K Tharun Sai <tharunsaik@users.noreply.github.com>
2020-11-25 23:04:38 +0530

    index

---
+++ index.php
@@ -0,0 +1,174 @@
+<!--
+Header Start
+-->
+<?php include 'header.php';?>
+<!--
+Header End
+-->
+<!--
+Main Content Start
+-->
+<!--
+Welcome Start
+-->
+<section class="bg-primary section-bg-right d-flex align-items-center ml-5 pl-5 uk-background-bottom-right uk-background-contain overflow-hidden" uk-height-viewport="offset-top: true" style="background-image: url(assets/img/bg-8.png);">
+	<div class="container-fluid">
+		<div class="row align-items-center">
+			<div class="col-sm-6">
+				<h1 class="text-white display-3">Get 10% Discount with us</h1>
+				<p class="text-white lead">Really Impressive offer for you!</p>
+				<a href="#" class="btn btn-warning">Get your card</a>
+			</div>
+		</div>
+	</div>
+</section>
+<!--
+Welcome End
+-->
+<!--
+Cards Start
+-->
+<section class="d-flex align-items-center uk-background-fixed overflow-hidden" uk-height-viewport="offset-top: true" style="background-image: url(assets/img/bg-9.png);">
+	<div class="container">
+		<div class="row align-items-center justify-content-center">
+			<div class="col-12 text-center mb-5">
+				<h1>Choose your card</h1>
+				<p class="lead">Here's all our cards, choose your one</p>
+			</div>
+			
+			<div class="col-sm-3 text-center">
+				<img src="assets/img/platinum.png" alt="" class="img-fluid">
+				<p>Plutinum Card</p>
+			</div>
+			<div class="col-sm-3 text-center">
+				<img src="assets/img/gold.png" alt="" class="img-fluid">
+				<p>Gold Card</p>
+			</div>
+			<div class="col-sm-3 text-center">
+				<img src="assets/img/thank-you.png" alt="" class="img-fluid">
+				<p>Thankyou Card</p>
+			</div>
+			<div class="col-sm-3 text-center">
+				<img src="assets/img/gift.png" alt="" class="img-fluid">
+				<p>Gift Card</p>
+			</div>
+		</div>
+	</div>
+</section>
+<!--
+Cards End
+-->
+<!--
+Wallet Start
+-->
+<div class="py-5">
+	<div class="container">
+		<div class="row text-center">
+			<div class="col-md-4 res-m-bttm-lg">
+				<div class="box-alt box-dot box-direction box-direction-one">
+					<div class="image-icon">
+						<img src="assets/img/icon-1.png" alt="box-icon">
+					</div>
+					<h5>Download a Wallet</h5>
+					<p>Download an app dolorts adipis sit ametcon sectetur.</p>
+				</div>
+			</div>
+			<div class="col-md-4 res-m-bttm-lg">
+				<div class="box-alt box-dot box-direction box-direction-two">
+					<div class="image-icon">
+						<img src="assets/img/icon-2.png" alt="box-icon">
+					</div>
+					<h5>Add Bitcoin to a Walet</h5>
+					<p>Once your wallet is  dolorts adipis sit ametcon sectetur.</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="box-alt box-dot">
+					<div class="image-icon">
+						<img src="assets/img/icon-3.png" alt="box-icon">
+					</div>
+					<h5>Send/Receive with Wallet</h5>
+					<p>The Process of sending dolorts adipis sit ametcon sectetur.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--
+Wallet End
+-->
+<!--
+Wallet Features Start
+-->
+<div class="py-5">
+<div class="container">
+	<div class="section-head">
+		<div class="row justify-content-center">
+			<div class="col-sm-auto text-center">
+				<h2>Why choose Us</h2>
+				<p>Sed ut perspi ciatis unde omnis iste natus error sit volup tatem accusa ntium dolor emque lauda ntium, totam rem aperiam</p>
+			</div>
+		</div>
+	</div>
+	<div class="row text-center">
+		<div class="col-md-4 col-sm-6">
+			<div class="box box-small-dot box-alt-bg round">
+				<span class="font-icon"><i class='lni lni-shield'></i></span>
+				<h4>Safe &amp; Secure</h4>
+				<p>Morbi eget varius risus, ut venenatis libero. Pellentesque in porta dui.</p>
+			</div>
+			<div class="gaps size-1-5x"></div>
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<div class="box box-small-dot box-alt-bg round">
+				<span class="font-icon"><span class="pe pe-7s-repeat"></span></span>
+				<h4>Recurring Buys</h4>
+				<p>Morbi eget varius risus, ut venenatis libero. Pellentesque in porta dui.</p>
+			</div>
+			<div class="gaps size-1-5x"></div>
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<div class="box box-small-dot box-alt-bg round">
+				<span class="font-icon"><span class="pe pe-7s-wallet"></span></span>
+				<h4>Mobile Apps</h4>
+				<p>Morbi eget varius risus, ut venenatis libero. Pellentesque in porta dui.</p>
+			</div>
+			<div class="gaps size-1-5x"></div>
+		</div>
+		<div class="col-md-4 col-sm-6 res-m-bttm">
+			<div class="box box-small-dot box-alt-bg round">
+				<span class="font-icon"><span class="pe pe-7s-cash"></span></span>
+				<h4>Low Commission</h4>
+				<p>Morbi eget varius risus, ut venenatis libero. Pellentesque in porta dui.</p>
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-6 res-m-bttm">
+			<div class="box box-small-dot box-alt-bg round">
+				<span class="font-icon"><span class="pe pe-7s-cart"></span></span>
+				<h4>Buy &amp; Sell</h4>
+				<p>Morbi eget varius risus, ut venenatis libero. Pellentesque in porta dui.</p>
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-6 res-m-bttm">
+			<div class="box box-small-dot box-alt-bg round">
+				<span class="font-icon"><span class="pe pe-7s-global"></span></span>
+				<h4>Global</h4>
+				<p>Morbi eget varius risus, ut venenatis libero. Pellentesque in porta dui.</p>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+<!--
+Wallet Features End
+-->
+<!--
+Main Content End
+-->
+<!--
+Footer Start
+-->
+<?php include 'footer.php';?>
+<!--
+Footer Start
+-->
